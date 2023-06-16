754616e7e297688f9cacb6c21bf3300e9e871ec7
Bột Coffee & Tea <113244777+BotCoffeeTea@users.noreply.github.com>
2023-06-17 06:50:53 +0700

    Update EventRecyclerAdapter.kt

--- LunarCalendar/app/src/main/java/com/dichvutaxi/lunarcalendar/adapters/EventRecyclerAdapter.kt
+++ LunarCalendar/app/src/main/java/com/dichvutaxi/lunarcalendar/adapters/EventRecyclerAdapter.kt
@@ -12,14 +12,14 @@ class EventRecyclerAdapter(
     private val context: Context,
     private val items: List<DateObject>,
     private val listener: OnItemClickListener
-) : RecyclerView.Adapter<AgendaEventViewHolder>() {
+) : RecyclerView.Adapter<EventViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AgendaEventViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.view_holder_agenda_event, parent, false)
-        return AgendaEventViewHolder(context, v)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.view_holder_event, parent, false)
+        return EventViewHolder(context, v)
     }
 
-    override fun onBindViewHolder(holder: AgendaEventViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         holder.bind(items[position], listener)
     }
 
