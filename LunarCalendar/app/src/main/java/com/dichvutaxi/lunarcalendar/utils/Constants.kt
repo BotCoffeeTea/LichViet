8d4a76a801533a55358168424d3e5c85430322c3
Bột Coffee & Tea <113244777+BotCoffeeTea@users.noreply.github.com>
2023-06-17 10:13:45 +0700

    Create Constants.kt

---
+++ LunarCalendar/app/src/main/java/com/dichvutaxi/lunarcalendar/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.dichvutaxi.lunarcalendar.utils
+
+object Constants {
+    val DAYS = arrayOf("",
+            "Chủ Nhật",
+            "Thứ Hai",
+            "Thứ Ba",
+            "Thứ Tư",
+            "Thứ Năm",
+            "Thứ Sáu",
+            "Thứ Bảy")
+}
