ea4496b7a6f19b078b431e855b837af1ebf7c12b
Bột Coffee & Tea <113244777+BotCoffeeTea@users.noreply.github.com>
2023-06-17 07:13:26 +0700

    Create OnItemClickListener.kt

---
+++ LunarCalendar/app/src/main/java/com/dichvutaxi/lunarcalendar/interfaces/OnItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.dichvutaxi.lunarcalendar.interfaces
+
+import com.dichvutaxi.lunarcalendar.models.EventObject
+
+interface OnItemClickListener {
+    fun onItemClick(item: EventObject)
+}
